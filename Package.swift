39be2544d9dba95e3136a6b77c914bb3f89b0fe1
Joe Heck <heckj@mac.com>
2020-03-05 16:02:02 -0800

    adding platforms for SwiftViz

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftViz",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_10),
+        .tvOS(.v13),
+        .watchOS(.v3)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
