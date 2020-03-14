5f26cef8531007257f48d77e3712010dedfbe1bc
Joseph Heck <heckj@mac.com>
2020-03-14 14:24:48 -0700

    Tick label (#4)
    * adding basic ticklabel to SwiftViz
    
    * makin' it public and writing validation logic
    
    * adding tests
    
    * formatting

---
+++ Sources/SwiftViz/TickLabel.swift
@@ -0,0 +1,52 @@
+//
+//  TickLabel.swift
+//  netlaband
+//
+//  Created by Joseph Heck on 3/11/20.
+//  Copyright © 2020 JFH Consulting. All rights reserved.
+//
+
+import CoreGraphics
+import Foundation
+
+/// A TickLabel is more specific to the visualization and
+/// is generally created with the benefit of a formatter,
+/// the type that we use being dependent on the type that's
+/// used for the domain of the Scale.
+public struct TickLabel: Identifiable {
+    public let id: UUID
+    public let rangeLocation: CGFloat
+    public let value: String
+
+    /// Full initializer for a TickLabel
+    /// - Parameters:
+    ///   - id: an instance of UUID
+    ///   - rangeLocation: location (CGFloat) of the label within the output range of a scale
+    ///   - value: String description of the label to be displayed
+    public init(id: UUID, rangeLocation: CGFloat, value: String) {
+        self.id = id
+        self.rangeLocation = rangeLocation
+        self.value = value
+    }
+
+    /// Convenience initializer for TickLabel
+    ///
+    /// - Parameters:
+    ///   - rangeLocation: location (CGFloat) of the label within the output range of a scale
+    ///   - value: String description of the label to be displayed
+    public init(rangeLocation: CGFloat, value: String) {
+        id = UUID()
+        self.rangeLocation = rangeLocation
+        self.value = value
+    }
+
+    /// static function on TickLabel that returns a default Formatter
+    /// object to assist in creating TickLabel instances
+    public static func makeDefaultFormatter() -> Formatter {
+        let defaultFormatter = NumberFormatter()
+        defaultFormatter.numberStyle = .decimal
+        defaultFormatter.minimumFractionDigits = 1
+        defaultFormatter.maximumFractionDigits = 2
+        return defaultFormatter
+    }
+}
--- Tests/SwiftVizTests/ExternalPackageTests.swift
+++ Tests/SwiftVizTests/ExternalPackageTests.swift
@@ -16,4 +16,24 @@ final class PackagingTests: XCTestCase {
         let ticks = scale.ticks([2.0], range: 0 ... 10.0)
         XCTAssertEqual(ticks.count, 1)
     }
+
+    func testManualTicksOutsideRange() {
+        let scale = LinearScale(domain: 0 ... 10.0, isClamped: false)
+        // verifies the method is visible externally - else this won't compile
+        let ticks = scale.ticks([2.0, 4.0, 8.0, 16.0], range: 0 ... 10.0)
+        XCTAssertEqual(ticks.count, 3)
+    }
+
+    func testManualTickLabelValidation() {
+        let scale = LinearScale(domain: 0 ... 10.0, isClamped: false)
+        // verifies the method is visible externally - else this won't compile
+        let manualLabels = [
+            TickLabel(rangeLocation: -1.0, value: "-1.0"),
+            TickLabel(rangeLocation: 1.0, value: "1.0"),
+            TickLabel(rangeLocation: 10.0, value: "10.0"),
+            TickLabel(rangeLocation: 100.0, value: "100.0"),
+        ]
+        let validatedSet = scale.validatedTickLabels(manualLabels, range: 0 ... 10.0)
+        XCTAssertEqual(validatedSet.count, 2)
+    }
 }
